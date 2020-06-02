bb7614a680c54a4b1914fd8ec3599b492fefc992
Mateusz Majak <mateuszmajak.mm@gmail.com>
2020-06-02 15:09:28 +0200

    Create new .R file for the future project

---
+++ small_project_main.R
@@ -0,0 +1,6 @@
+############################################################
+######## Machine Learning 1 Project - Small dataset ########
+########     Kenneth Petrykowski & Mateusz Majak    ########
+############################################################
+
+print("Hello Machine Learning, we're glad You'll be in our CVs")
