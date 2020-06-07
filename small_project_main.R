4371e5cf86f350e7e315f3680933ce02c9078cca
Mateusz Majak <mateuszmajak.mm@gmail.com>
2020-06-07 23:32:51 +0200

    first quick clean up (just a little bit)

--- small_project_main.R
+++ small_project_main.R
@@ -3,4 +3,31 @@
 ########     Kenneth Petrykowski & Mateusz Majak    ########
 ############################################################
 
-print("Hello Machine Learning, we're glad You'll be in our CVs")
+#getwd()
+#setwd("C:/Users/Admin/Documents/GIT/ProjectsML1")
+coffee <- read.csv("arabica_data_cleaned.csv")
+
+#library(dplyr)
+glimpse(coffee)
+
+coffee$X                     <- NULL
+coffee$Species               <- NULL
+coffee$Certification.Address <- NULL
+coffee$Certification.Contact <- NULL
+coffee$Lot.Number            <- NULL
+coffee$Mill                  <- NULL
+coffee$ICO.Number            <- NULL
+coffee$Expiration            <- NULL
+coffee$Grading.Date          <- NULL
+coffee$Company               <- NULL
+coffee$Farm.Name             <- NULL
+coffee$Owner                 <- NULL
+coffee$Producer              <- NULL
+coffee$Owner.1               <- NULL
+coffee$In.Country.Partner    <- NULL
+coffee$Region                <- NULL
+
+table(coffee$Quakers)
+table(coffee$Category.One.Defects)
+table(coffee$Category.Two.Defects)
+
