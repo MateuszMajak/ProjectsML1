d03c65d02d9e0cf3b563b4114de47c4306d6bc48
Mateusz Majak <mateuszmajak.mm@gmail.com>
2020-06-09 16:38:16 +0200

    kilka nowych linijek kodu

--- small_project_main.R
+++ small_project_main.R
@@ -2,7 +2,15 @@
 ######## Machine Learning 1 Project - Small dataset ########
 ########     Kenneth Petrykowski & Mateusz Majak    ########
 ############################################################
-
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(caret)
+library(tibble)
+library(purrr)
+library(corrplot)
+library(DescTools)
+library(olsrr)
 #getwd()
 #setwd("C:/Users/Admin/Documents/GIT/ProjectsML1")
 coffee <- read.csv("arabica_data_cleaned.csv")
@@ -26,8 +34,73 @@ coffee$Producer              <- NULL
 coffee$Owner.1               <- NULL
 coffee$In.Country.Partner    <- NULL
 coffee$Region                <- NULL
+coffee$Variety               <- NULL
+coffee$Altitude              <- NULL
+coffee$Certification.Body    <- NULL
 
-table(coffee$Quakers)
+### !!!!!!!!! ###
+# Do przemyœlenia:
 table(coffee$Category.One.Defects)
 table(coffee$Category.Two.Defects)
+table(coffee$Processing.Method)
 
+# Countung NAs in every column
+colSums(is.na(coffee)) %>% 
+  sort()
+
+
+# Let's start with the column Quakers. There is one NA
+table(coffee$Quakers, useNA = "ifany")
+
+#We should probably replace the missing with the median
+median(coffee$Quakers, na.rm=TRUE)
+coffee$Quakers[is.na(coffee$Quakers)] <- 0
+
+
+table(coffee$altitude_low_meters, useNA = "ifany")
+table(coffee$altitude_high_meters, useNA = "ifany")
+table(coffee$altitude_mean_meters, useNA = "ifany")
+
+ggplot(coffee,
+       aes(x = Total.Cup.Points)) +
+  geom_histogram(fill = "blue",
+                 bins = 100) +
+  theme_bw()
+
+# we delete outlier (Total.Cup.Points==0)
+coffee <- coffee[!(coffee$Total.Cup.Points==0),]
+
+
+###
+#####
+####### Now let's see which independent variables should we choose
+
+
+coffees_numeric_vars <- 
+  sapply(coffee, is.numeric) %>% 
+  which() %>% 
+  names()
+
+coffees_numeric_vars
+
+
+coffees_correlations <- 
+  cor(coffee[,coffees_numeric_vars],
+      use = "pairwise.complete.obs")
+
+coffees_correlations
+
+corrplot(coffees_correlations, 
+         method = "pie")
+
+coffee_lm1 <- lm(Total.Cup.Points ~ Aroma,
+                 data = coffee)
+
+summary(coffee_lm1)
+
+
+houses_lm2 <- lm(Total.Cup.Points ~ Aroma + Flavor + Acidity +
+                   Body + Balance + Sweetness + Category.One.Defects +Category.Two.Defects,
+                 data = coffee)
+
+summary(houses_lm2)
