94c69ef12cbea38672edf09645e2e6633da57c65
Mateusz Majak <mateuszmajak.mm@gmail.com>
2020-08-04 12:49:59 +0200

    creating new R file for the classification models

---
+++ big_project/big_project_main.R
@@ -0,0 +1,14 @@
+############################################################
+########  Machine Learning 1 Project - Big dataset  ########
+########     Kenneth Petrykowski & Mateusz Majak    ########
+############################################################
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(caret)
+library(tibble)
+library(purrr)
+library(corrplot)
+library(DescTools)
+library(olsrr)
+#getwd()
